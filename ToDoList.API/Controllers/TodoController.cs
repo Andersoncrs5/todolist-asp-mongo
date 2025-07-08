9fb2116126fa2018be5d3bef7dfa122441b8a67b
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-08 16:56:34 -0300

    Added controllers

---
+++ ToDoList.API/Controllers/TodoController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.API.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet("/get-all")]
+        public async Task<IActionResult> getAll() {
+            return Ok("All Tasks");
+        }
+    }
+}
