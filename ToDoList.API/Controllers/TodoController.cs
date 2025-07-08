3940ec4ce725e67a423f988644994ad02ef32754
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-08 19:15:53 -0300

    Added controller

--- ToDoList.API/Controllers/TodoController.cs
+++ ToDoList.API/Controllers/TodoController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.API.Contracts.Repositories;
+using ToDoList.API.models;
+using ToDoList.API.SetUnitOfWork;
 
 namespace ToDoList.API.Controllers
 {
@@ -11,9 +14,25 @@ namespace ToDoList.API.Controllers
     [Route("api/v{version:apiVersion}/[controller]")]
     public class TodoController : ControllerBase
     {
-        [HttpGet("/get-all")]
-        public async Task<IActionResult> GetAll() {
-            return Ok("All Tasks");
+        private readonly IUnitOfWork _unit;
+
+        public TodoController(IUnitOfWork unit)
+        {
+            _unit = unit;
         }
+
+        [HttpGet("/get-all")]
+        public async Task<IActionResult> GetAll() 
+        {
+            var result = await _unit.TaskRepository.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] TaskModel task)
+        {
+            
+        }
+
     }
 }
