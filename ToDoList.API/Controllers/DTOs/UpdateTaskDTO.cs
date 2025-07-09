7c13f376685e25530c5149a5d33098c1c90c9f91
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-07-09 12:24:19 -0300

    Validation in DTO

--- ToDoList.API/Controllers/DTOs/UpdateTaskDTO.cs
+++ ToDoList.API/Controllers/DTOs/UpdateTaskDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,12 @@ namespace ToDoList.API.Controllers.DTOs
 {
     public class UpdateTaskDTO
     {
+        [Required(ErrorMessage = "Task name is required.")] 
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "The max is 50 and min is 5")]
         public string Name { get; set; } = string.Empty;
+
+        [StringLength(200, ErrorMessage = "The max is 200")]
         public string Description { get; set; } = string.Empty;
         public bool IsComplete { get; set; } = false;
-
     }
 }
